a79591beb297ce323043074d32899020c9296deb
CharlieAIO <39866368+CharlieAIO@users.noreply.github.com>
2019-11-06 23:41:01 +0000

    Update sup.py

--- sup.py
+++ sup.py
@@ -35,3 +35,7 @@ def main():
         webhook_name.set_footer(text='CharlieAIO | Supreme Droplist')
         Hook.send(embed=webhook_name)
         print("| WEBHOOK SENT |")
+
+        
+if __name__ == "__main__":
+    main()
